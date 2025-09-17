09a4cdf9a4009f881833a9bf48ab4dad9093c294
Alex Hagemeister <xanhagem@berkeley.edu>
2025-09-17 00:49:29 -0700

    Initial Commit

---
+++ kairoscope/kairoscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  kairoscopeApp.swift
+//  kairoscope
+//
+//  Created by alex hagemeister on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct kairoscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
